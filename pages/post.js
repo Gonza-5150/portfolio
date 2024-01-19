065204c2e26d7877c8325e9ab04ce1166fb60912
Gonza-5150 <estevesgonzalo@hotmail.com>
2024-01-19 00:15:12 +0000

    deploy

--- pages/post.js
+++ pages/post.js
@@ -1,3 +1,31 @@
+//import Layout from "../components/Layout";
+//import { useRouter } from "next/router";
+//import { posts } from "../profile";
+
+// const Post = () => {
+//   const router = useRouter();
+
+//   const currentPost = posts.filter(
+//     (post) => post.title === router.query.title
+//   )[0];
+
+//   return (
+//     <Layout title={router.query.title} footer={false}>
+//       <div className="text-center">
+//         <img
+//           src={currentPost.imageURL}
+//           alt=""
+//           style={{ width: "50%" }}
+//           className="img-fluid"
+//         />
+//         <p className="p-4"><strong>{currentPost.content}</strong></p>
+//       </div>
+//     </Layout>
+//   );
+// };
+
+// export default Post;
+
 import Layout from "../components/Layout";
 import { useRouter } from "next/router";
 import { posts } from "../profile";
@@ -5,9 +33,15 @@ import { posts } from "../profile";
 const Post = () => {
   const router = useRouter();
 
-  const currentPost = posts.filter(
+  const currentPost = posts.find(
     (post) => post.title === router.query.title
-  )[0];
+  );
+
+  // Verifica si currentPost existe antes de renderizar
+  if (!currentPost) {
+    // Puedes renderizar un mensaje de error o redirigir al usuario
+    return <div>Post no encontrado</div>;
+  }
 
   return (
     <Layout title={router.query.title} footer={false}>
